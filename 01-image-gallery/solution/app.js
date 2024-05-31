cad24bcba81966abf9e82b01a4e6adc19b7699e5
wolfgunblood <jalajdorai1@gmail.com>
2024-05-31 06:26:24 +0530

    done with error handling

--- 01-image-gallery/solution/app.js
+++ 01-image-gallery/solution/app.js
@@ -3,11 +3,14 @@ const app = express()
 require('dotenv').config()
 const images = require('./controllers/images')
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //routes
 
 app.use('/api/v1/getimages',images)
 app.use(notFound)
+app.use(errorHandlerMiddleware);
+
 
 const PORT = process.env.PORT || 3000
 
--- 01-image-gallery/solution/controllers/images.js
+++ 01-image-gallery/solution/controllers/images.js
@@ -1,7 +1,10 @@
 const asyncWrapper = require("../middleware/async");
-
 const getImages = asyncWrapper(async (req, res) => {
-  res.status(200).send("Get all images");
+   
+    k
+    return res.status(200).send("Get all images");
 });
+
+
 
 module.exports = getImages;
